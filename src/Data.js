cbf0002c92b769892aa44e72236f824a6708fd9a
jscodelover <manisha.4918@gmail.com>
2019-05-26 13:55:24 +0530

    added data for discography

---
+++ src/Data.js
@@ -0,0 +1,108 @@
+export const album = [
+  {
+    name: "carefree",
+    id: 0
+  },
+  {
+    name: "childhood",
+    id: 1
+  },
+  {
+    name: "dance and pop",
+    id: 2
+  }
+];
+
+export const albumDetail = [
+  {
+    id: 0,
+    albumName: "carefree",
+    year: 2015,
+    tracks: [
+      {
+        id: "767",
+        name: "Bluegrass Billy",
+        detail:
+          "a simple, fun and carefree track with bluegrass banjo melody, upright bass, drums and hand claps",
+        url:
+          "https://www.zapsplat.com/wp-content/uploads/2015/music-one/music_zapsplat_blugrass_billy.mp3?_=1"
+      },
+      {
+        id: "123",
+        name: "Rascals",
+        detail:
+          "fun, lighthearted yet cheeky music ideal for conveying carefree play, fast strummed banjo and acoustic guitar, double bass, hand claps and a steel string guitar riff on top",
+        url:
+          "https://www.zapsplat.com/wp-content/uploads/2015/music-one/music_zapsplat_rascals_123.mp3?_=2"
+      },
+      {
+        id: "693",
+        name: "It Takes One",
+        detail:
+          "fun, light-hearted acoustic guitar melody with light percussion and piano rhythm with a tropical feel",
+        url:
+          "https://www.zapsplat.com/wp-content/uploads/2015/music-one/jes_smith_music_it_takes_one.mp3?_=3"
+      }
+    ]
+  },
+  {
+    id: 2,
+    albumName: "childhood",
+    year: 2015,
+    tracks: [
+      {
+        id: "154",
+        name: "Butterfly",
+        detail:
+          "fun children’s music with a playful and inquisitive feel with piano, marimba and acoustic bass",
+        url:
+          "https://www.zapsplat.com/wp-content/uploads/2015/music-one/music_zapsplat_butterfly_101.mp3?_=3"
+      },
+      {
+        id: "784",
+        name: "Show And Tell",
+        detail: "playful and fun children’s music with a tropical feel",
+        url:
+          "https://www.zapsplat.com/wp-content/uploads/2015/music-one/audio_hero_Show-And-Tell_SIPML_Q-0149.mp3?_=5"
+      },
+      {
+        id: "457",
+        name: "Story Time",
+        detail:
+          "nursery rhyme themed fun children’s music covering elements of many classic nursery rhymes",
+        url:
+          "https://www.zapsplat.com/wp-content/uploads/2015/music-one/audio_hero_Story-Time_SIPML_Q-0216.mp3?_=6"
+      }
+    ]
+  },
+  {
+    id: 3,
+    albumName: "Dance and pop",
+    year: 2015,
+    tracks: [
+      {
+        id: "254",
+        name: "Help You Out",
+        detail: "electronic EDM with elements of trap, electronic growling bass",
+        url:
+          "https://www.zapsplat.com/wp-content/uploads/2015/music-one/music_leonell_cassio_help_you_out_ft_jonathon_robbins.mp3?_=1"
+      },
+      {
+        id: "848",
+        name: "Good Times",
+        detail:
+          "uplifting, positive house with a pumping 4/4 beat, electric piano chords, warm synth pads and a happy, bright piano melody",
+        url:
+          "https://www.zapsplat.com/wp-content/uploads/2015/music-one/music_zapsplat_good_times.mp3?_=2"
+      },
+      {
+        id: "325",
+        name: "Headliner",
+        detail:
+          "euphoric trance, house, bright synth arpeggio loop and drums lead into an uplifting euphoric chorus",
+        url:
+          "https://www.zapsplat.com/wp-content/uploads/2015/music-one/music_zapsplat_headliner_130.mp3?_=3"
+      }
+    ]
+  }
+];
